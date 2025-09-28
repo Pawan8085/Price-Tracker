94a30e5f9b276dc1f66dba765ed48b285e056466
Pawan <kumarpawanm8085@gmail.com>
2025-09-28 22:55:40 +0530

    added spring security

---
+++ PriceTracker/src/main/java/com/app/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+
+        http.csrf(customizer -> customizer.disable());
+        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults()); // for browser
+        http.httpBasic(Customizer.withDefaults());// for rest api
+
+        return http.build();
+    }
+}
